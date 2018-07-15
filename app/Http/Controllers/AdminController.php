773786be677789c63040750e23122e979c4802b9
shababhsiddique <shabab.h.siddique@gmail.com>
2018-07-15 18:14:47 +0600

    Added Login template, added db for admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class SiteController extends Controller
+class AdminController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,9 +13,8 @@ class SiteController extends Controller
      */
     public function index()
     {
-        return view('site.master');
+        //
     }
-    
     
 
     /**
